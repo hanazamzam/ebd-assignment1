89be35d449ef33a3ba3523b1ffc36250f3b82845
Hana <hana.zamzam@student.giu-uni.de>
2026-09-23 00:32:59 +0300

    Add solution

---
+++ assignment/solution.js
@@ -0,0 +1,8 @@
+let heroName = "Hana the Mage";
+let studentID = 16001960;
+let power = studentID * 3;
+
+console.log(heroName);
+console.log(studentID);
+console.log(power);
+console.log("By the Sinai dunes, the Ash Phoenix burns out!");
